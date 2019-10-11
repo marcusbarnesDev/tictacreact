d2f07f25a1e62625395104771457878dd9ce4afd
Marcus <you@example.com>
2019-10-11 12:29:28 -0700

    init commit

---
+++ precache-manifest.72d841569852ea6fbf7b5b5215fa66d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1006f85016fac1b06db24fcf432ad186",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9bb52b3831407681136",
+    "url": "/static/css/main.799033a2.chunk.css"
+  },
+  {
+    "revision": "01586c15531a01a31af4",
+    "url": "/static/js/2.1f803099.chunk.js"
+  },
+  {
+    "revision": "c9bb52b3831407681136",
+    "url": "/static/js/main.2b5be6c0.chunk.js"
+  },
+  {
+    "revision": "fc086f0bcdf27f5ed993",
+    "url": "/static/js/runtime-main.ee40dbad.js"
+  }
+]);
